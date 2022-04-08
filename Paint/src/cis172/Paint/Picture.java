de3eb27bf4a0ea354d92c172807142eae3782558
IMadeAUsername1 <102881663+IMadeAUsername1@users.noreply.github.com>
2022-04-08 15:13:01 -0400

    Moving some code I had in Frame to Picture

--- Paint/src/cis172/Paint/Picture.java
+++ Paint/src/cis172/Paint/Picture.java
@@ -1,8 +1,13 @@
 package cis172.Paint;
 import java.util.ArrayList;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
+
+import cis172.Paint.Picture.ToolOpt;
 
 public class Picture extends JPanel {
 	
@@ -28,6 +33,53 @@ public class Picture extends JPanel {
 		currentColor = Color.black;
 		currentWidth=10;
 		currentTool = null; 
+		
+		if (currentTool == ToolOpt.RECTANGLE) { 
+			Rectangle r1 = new Rectangle();
+			
+			addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					r1.setX(e.getX());
+					r1.setY(e.getY());
+					
+				}
+				
+				public void mouseReleased(MouseEvent e) {
+					r1.setWidth(e.getX() - r1.getX());
+					r1.setHeight(e.getY() - r1.getY());
+					
+					shapes.add(r1);
+					r1.draw(getGraphics());
+					
+				}
+				
+			});
+			
+			
+		} else if (currentTool == ToolOpt.CIRCLE) { 
+			Circle c1 = new Circle();
+			
+			addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					c1.setX(e.getX());
+					c1.setY(e.getY());
+					
+				}
+				
+				public void mouseReleased(MouseEvent e) {
+					c1.setWidth(e.getX() - c1.getX());
+					c1.setHeight(e.getY() - c1.getY());
+					
+					shapes.add(c1);
+					c1.draw(getGraphics());
+					
+				}
+				
+			});
+			
+			
+		}
+		
 	}
 	
 	public void export() {
@@ -62,6 +114,7 @@ public class Picture extends JPanel {
 	public void setCurrentWidth(int currentWidth) {
 		this.currentWidth = currentWidth; 
 	}
+	
 	
 	
 }
