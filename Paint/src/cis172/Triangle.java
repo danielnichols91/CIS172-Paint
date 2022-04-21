0d01cca9247f6397a196ec1f940945878f21fda6
IMadeAUsername1 <102881663+IMadeAUsername1@users.noreply.github.com>
2022-04-20 22:59:33 -0400

    Fixing Triangle

--- Paint/src/cis172/Triangle.java
+++ Paint/src/cis172/Triangle.java
@@ -13,15 +13,22 @@ public class Triangle extends Shape{
 		if(getColor()==null) {
 			throw new NullPointerException("Current Color is null and has not been selected");
 		}
-		int x3; 
+		
+		int y1;
+		int y2;
 		int y3;
 		
-		x3 = Math.abs(getWidth() - (getX() - getWidth()));
-		y3 = Math.abs(getHeight() - getY());
+		if (getY() > getHeight()) {
+			y1 = getHeight();
+			y2 = y3 = getY();
+		} else {
+			y1 = getY();
+			y2 = y3 = getHeight();
+		}
 			
 		g.setColor(getColor());
 		g2d.setStroke(new BasicStroke(getThickness()));
-		g.drawPolygon(new int[] {getX(), getWidth(), x3}, new int[] {getY(), getHeight(), y3}, 3);
+		g.drawPolygon(new int[] {getX(), getWidth(), Math.abs(getWidth() - ((getWidth() - getX()) * 2))}, new int[] {y1, y2, y3}, 3);
 	}
 
 }
